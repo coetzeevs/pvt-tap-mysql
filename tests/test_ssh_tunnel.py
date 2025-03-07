496338a672466523a821c3cbf0ff627974a26626
Coetzee van Staden <cstaden@credable.io>
2025-03-07 16:56:59 +0000

    update(workflows): zeroing in more???

--- tests/test_ssh_tunnel.py
+++ tests/test_ssh_tunnel.py
@@ -1,7 +1,9 @@
 """Tests standard tap features using the built-in SDK tests library."""
 
+import logging
 from pathlib import Path
 
+import paramiko
 from tap_mysql.tap import TapMySQL
 
 TABLE_NAME = "test_replication_key"
@@ -19,7 +21,16 @@ SAMPLE_CONFIG = {
 
 def test_ssh_tunnel() -> None:
     """We expect the SSH environment to already be up."""
-    private_key = Path("ssh_tunnel/ssh-server-config/ssh_host_rsa_key").read_text()
+    # Configure paramiko logging
+    paramiko.util.log_to_file("paramiko.log")
+    logging.basicConfig(level=logging.DEBUG)
+    paramiko_logger = logging.getLogger("paramiko")
+    paramiko_logger.setLevel(logging.DEBUG)
+
+    # Read the key *contents* from the file.  This avoids permission issues.
+    key_path = Path(__file__).parent / "ssh_tunnel" / "ssh-server-config" / "ssh_host_rsa_key"
+    private_key = key_path.read_text()
+
     SAMPLE_CONFIG["ssh_tunnel"]["private_key"] = private_key
     tap = TapMySQL(config=SAMPLE_CONFIG)
     tap.sync_all()
